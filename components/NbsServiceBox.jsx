c3269c570bb9887b1d7902d874c28619feefa0ae
Christerresin <christer.resin@gmail.com>
2022-08-15 21:45:00 +0200

    update text for NBS service box

--- components/NbsServiceBox.jsx
+++ components/NbsServiceBox.jsx
@@ -7,7 +7,7 @@ const service = {
   left_content:
     'Vill du inte montera och installera systemet själv? I så fall kan vi erbjuda personlig installation av ditt system, tillsammans med dig diskuterar vi möjliga placeringar av systemets sensorer och styrenheter på båten. Vi gör en uppskattning av tidsåtgången och erbjuder ett fast totalpris, i installationen ingår utbildning av systemet och dess funktioner.',
   left_content_addon:
-    'För uppkoppling mot Westra Securitys larmcentral krävs att systemet är installerat och funktionen verifierad av en av Nordic Boat Security auktoriserad installatör.',
+    'För uppkoppling mot Westra Securitys larmcentral krävs att systemet är installerat och funktionen verifierad antingen av Nordic Boat Security eller en av Nordic Boat Security auktoriserad installatör',
 };
 
 function NbsServiceBox({ grid }) {
