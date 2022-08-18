c6b85d01847ae3a1cab036698e22b28c8195ad9f
Christerresin <christer.resin@gmail.com>
2022-08-18 08:53:19 +0200

    refactor logic for gSP to fetch correct data

--- pages/produkter/[slug].js
+++ pages/produkter/[slug].js
@@ -3,7 +3,7 @@ import { productList } from '../../productList';
 export const getStaticPaths = async () => {
   const paths = productList.map((product) => ({
     params: {
-      slug: product.id.toString(),
+      slug: product.productName,
     },
   }));
 
@@ -17,23 +17,26 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = ({ params: { slug } }) => {
   console.log(slug);
-  const data = productList.find((product) => product.id === parseInt(slug));
+  const data = productList.find((product) => product.productName === slug);
   console.log(data);
 
   const title = data.title;
+  const content = data.content;
 
   return {
     props: {
       title: title,
+      content: content,
     },
   };
 };
 
-const Produkt = ({ title }) => {
+const Produkt = ({ title, content }) => {
   return (
     <div>
       <h1>Produktsida</h1>
       <h3>{title}</h3>
+      <p>{content}</p>
     </div>
   );
 };
