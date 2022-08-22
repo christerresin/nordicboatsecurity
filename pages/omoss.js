fc31487dad295e766edccd5d08fa2955a61d9e2a
Christerresin <christer.resin@gmail.com>
2022-08-22 20:45:52 +0200

    add contact info to omoss page

--- pages/omoss.js
+++ pages/omoss.js
@@ -1,10 +1,13 @@
 import Head from 'next/head';
 import About from '../components/About';
 import Bottom from '../components/Bottom';
+import ContactBox from '../components/ContactBox';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Top from '../components/Top';
+
 import styles from '../styles/Home.module.scss';
+import { contactList } from '../contactList';
 
 export default function omoss() {
   return (
@@ -27,8 +30,11 @@ export default function omoss() {
         }
       />
       <About grid='grid7' />
-      <Bottom grid='grid9' />
-      <Footer grid='grid10' />
+      {contactList.map((contact, index) => (
+        <ContactBox key={index} grid={'grid' + (9 + index)} contact={contact} />
+      ))}
+      <Bottom grid='grid11' />
+      <Footer grid='grid12' />
     </div>
   );
 }
