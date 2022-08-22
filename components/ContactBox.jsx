33fb48c878f05230eaf70908f33eefa2582e98e6
Christerresin <christer.resin@gmail.com>
2022-08-22 20:24:19 +0200

    add icons to kontakt page

--- components/ContactBox.jsx
+++ components/ContactBox.jsx
@@ -1,3 +1,6 @@
+import Mail from '@mui/icons-material/MailOutlined';
+import Call from '@mui/icons-material/CallOutlined';
+
 import styles from '../styles/ContactBox.module.scss';
 
 function ContactBox({ grid, contact }) {
@@ -8,8 +11,12 @@ function ContactBox({ grid, contact }) {
           {contact.name}
           <span className={styles.role}>, {contact.role}</span>
         </p>
-        <p className={styles.phone}>{contact.phone}</p>
+        <p className={styles.phone}>
+          <Call className={styles.icon} sx={{ fontSize: '16px' }} />
+          {contact.phone}
+        </p>
         <a href={`mailto: ${contact.email}`} className={styles.email}>
+          <Mail className={styles.icon} sx={{ fontSize: '16px' }} />
           {contact.email}
         </a>
       </div>
