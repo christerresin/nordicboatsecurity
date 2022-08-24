3a44b1f556c40f9a25dfb442f6595419c727ca69
Christerresin <christer.resin@gmail.com>
2022-08-24 14:50:15 +0200

    add links to tjänster page from Services with id location

--- servicesList.js
+++ servicesList.js
@@ -3,7 +3,7 @@ export const servicesList = [
     image: '/gps_800.jpg',
     imgAlt: 'gps tracking',
     header: 'Övervakning i realtid',
-    url: '/tjanster',
+    url: '/tjanster#gost_services',
     content:
       'Systemen rapporterar kontinuerligt in information och data som du när som helst kan ta del av via din dator, surfplatta eller smartphone.',
   },
@@ -11,14 +11,14 @@ export const servicesList = [
     image: '/jetski_800.jpg',
     imgAlt: 'alarm emergency',
     header: 'Larmutryckning med vaktbolag',
-    url: '/tjanster',
+    url: '/tjanster#westra_services',
     content:
       'Tillsammans med det ISO-, och SSF-certifierade säkerhetsbolaget Westra Security kan vi erbjuda övervakning av din båt. Givetvis med möjlighet till väktarutryckning! Dygnet runt, året om!',
   },
   {
     image: '/install_800.jpg',
     header: 'Personlig installation av ditt system',
-    url: '/tjanster',
+    url: '/tjanster#nbs_services',
     imgAlt: 'personal installation',
     content:
       'Vill du inte montera och installera systemet själv? I så fall kan vi erbjuda personlig installation av ditt system, tillsammans med dig diskuterar vi möjliga placeringar av systemets sensorer och styrenheter på båten.',
