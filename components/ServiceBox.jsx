02a493ef6f95f155aedfb5949dc2d52d62e39163
Christerresin <christer.resin@gmail.com>
2022-08-24 13:34:30 +0200

    fix

--- components/ServiceBox.jsx
+++ components/ServiceBox.jsx
@@ -1,13 +1,17 @@
+import Link from 'next/link';
+
 import styles from '../styles/ServiceBox.module.scss';
 
 function ServiceBox({ header, image, imgAlt, content, url }) {
   return (
     <div className={styles.box_container}>
-      <a href={url} className={styles.container}>
-        <img className={styles.img} alt={imgAlt} src={image}></img>
-        <h3>{header}</h3>
-        <p>{content}</p>
-      </a>
+      <Link href={url}>
+        <a className={styles.container}>
+          <img className={styles.img} alt={imgAlt} src={image}></img>
+          <h3>{header}</h3>
+          <p>{content}</p>
+        </a>
+      </Link>
     </div>
   );
 }
