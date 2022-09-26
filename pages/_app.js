4f78c663033dfa29bd4fdccfd9111701bf272b24
Christerresin <christer.resin@gmail.com>
2022-09-26 13:13:17 +0200

    add conditional in app to check if /cookiepolicy page

--- pages/_app.js
+++ pages/_app.js
@@ -3,58 +3,62 @@ import Layout from '../components/layout';
 import { getCookie } from 'cookies-next';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, ...appProps }) {
   const consent = getCookie('localConsent');
-  return (
-    <>
-      <Script
-        id='gtag'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-
-        gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'analytics_storage': 'denied'
-        });
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-P7Q45JF');`,
-        }}
-      />
-
-      {consent === true && (
+  if (appProps.router.pathname === '/cookiepolicy') {
+    return <Component {...pageProps} />;
+  } else {
+    return (
+      <>
         <Script
-          id='consupd'
+          id='gtag'
           strategy='afterInteractive'
           dangerouslySetInnerHTML={{
             __html: `
-            gtag('consent', 'update', {
-              'ad_storage': 'granted',
-              'analytics_storage': 'granted'
-            });
-          `,
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+
+          gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'analytics_storage': 'denied'
+          });
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-P7Q45JF');`,
           }}
         />
-      )}
 
-      <noscript>
-        <iframe
-          src='https://www.googletagmanager.com/ns.html?id=GTM-P7Q45JF'
-          height='0'
-          width='0'
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+        {consent === true && (
+          <Script
+            id='consupd'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+              gtag('consent', 'update', {
+                'ad_storage': 'granted',
+                'analytics_storage': 'granted'
+              });
+            `,
+            }}
+          />
+        )}
+
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-P7Q45JF'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    );
+  }
 }
 
 export default MyApp;
