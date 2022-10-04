a3e2b439ce777b79c6c0741447a047602c71d00e
Christerresin <christer.resin@gmail.com>
2022-10-04 08:40:21 +0200

    move Script to document and change strategy

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,31 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <Script
+          strategy='beforeInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-P7Q45JF`}
+        />
+        <Script
+          id='gtag'
+          strategy='beforeInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+
+        gtag('consent', 'default', {
+          'ad_storage': 'denied',
+          'analytics_storage': 'denied'
+        });
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE}');`,
+          }}
+        />
+
         <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE}`}
