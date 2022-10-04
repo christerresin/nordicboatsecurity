13ff76ba5d142434eea2d7bf80f6285c4f578511
Christerresin <christer.resin@gmail.com>
2022-10-04 10:13:42 +0200

    replace Next Script with script in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
+import Script from 'next/script';
+import Layout from '../components/layout';
+import { getCookie } from 'cookies-next';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, ...appProps }) {
-  return <Component {...pageProps} />;
+  const consent = getCookie('localConsent');
+  if (appProps.router.pathname === '/cookiepolicy') {
+    return <Component {...pageProps} />;
+  } else {
+    return (
+      <>
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-P7Q45JF`}
+        />
+
+        {consent === true && (
+          <Script
+            id='consupd'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+              gtag('consent', 'update', {
+                'ad_storage': 'granted',
+                'analytics_storage': 'granted'
+              });
+            `,
+            }}
+          />
+        )}
+
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </>
+    );
+  }
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,36 @@ export default function Document() {
     <Html lang='sv'>
       <Head>
         <link rel='icon' href='/favicon.webp' />
+        <script
+          id='gtag'
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied'
+  });
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE}');`,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE}`}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </Html>
   );
