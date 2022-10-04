3a7cd3b3be2296d428f724181d5a9b7cc81abac7
Christerresin <christer.resin@gmail.com>
2022-10-04 08:44:10 +0200

    move Script to _app and change strategy

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,34 @@ function MyApp({ Component, pageProps, ...appProps }) {
   } else {
     return (
       <>
+        <Script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-P7Q45JF`}
+        />
+        <Script
+          id='gtag'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+
+    gtag('consent', 'default', {
+      'ad_storage': 'denied',
+      'analytics_storage': 'denied'
+    });
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE}');`,
+          }}
+        />
+
         {consent === true && (
           <Script
             id='consupd'
-            strategy='lazyOnload'
+            strategy='afterInteractive'
             dangerouslySetInnerHTML={{
               __html: `
               gtag('consent', 'update', {
