98ab2f2e42eaece91e139fa2e971c5018e52a142
Christerresin <christer.resin@gmail.com>
2022-10-21 10:09:46 +0200

    refactor rules for News scss

--- components/News.jsx
+++ components/News.jsx
@@ -6,16 +6,16 @@ function News({ grid }) {
       <div className={styles.usp}>
         <div className={styles.text}>
           <h2>Våra produkter fortsätter vinna priser!</h2>
-          <h4 className={styles.bold}>
+          <h4>
             Gost® wins fifth NMEA product of excellence award in remote vessel
             monitoring category.
           </h4>
-          <p className={styles.italic}>
+          <p>
             The Product of Excellence Awards are voted on by professional marine
             electronics installing NMEA dealers, qualified NMEA technicians and
             fellow marine electronics manufacturers.
           </p>
-          <p className={styles.italic}>
+          <p>
             The Apparition is an all-in-one security, tracking and monitoring
             system. It is designed to protect the vessel whether an owner is
             aboard or afar, provide complete situational awareness, and send
