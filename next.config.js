27afd61b8769e24dde2ca22f8c21aabf420510a8
Christerresin <christer.resin@gmail.com>
2022-11-16 16:03:21 +0100

    update next config for env

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
       unoptimized: true,
     },
   },
+  env: {
+    NEXT_PUBLIC_MAPS: process.env.NEXT_PUBLIC_MAPS,
+    NEXT_PUBLIC_GOOGLE: process.env.NEXT_PUBLIC_GOOGLE,
+  },
 };
 
 module.exports = nextConfig;
