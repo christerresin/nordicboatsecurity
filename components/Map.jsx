4223da0cab2d3a8aa55a3021974183d4990a3d10
Christerresin <christer.resin@gmail.com>
2022-11-16 14:41:36 +0100

    add crude google places component

--- components/Map.jsx
+++ components/Map.jsx
@@ -8,21 +8,52 @@ import {
   MarkerF,
   InfoWindowF,
 } from '@react-google-maps/api';
-import { useState } from 'react';
+
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from 'react-places-autocomplete';
+import PlacesAutocomplete from 'react-places-autocomplete';
+
+import { useEffect, useState } from 'react';
 
 function Map({ grid }) {
+  const [selectedRetailer, setSelectedRetailer] = useState(null);
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [center, setCenter] = useState({ lat: 65.58456, lng: 22.14785 });
+  const [address, setAddress] = useState('');
+  const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+  const googleLibraries = ['places'];
+  const searchOptions = {
+    componentRestrictions: { country: ['se'] },
+  };
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const ll = await getLatLng(results[0]);
+    console.log(ll);
+    setAddress(value);
+    setCoordinates(ll);
+    setCenter(ll);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS,
+    libraries: googleLibraries,
   });
-
-  const [selectedRetailer, setSelectedRetailer] = useState(null);
-
-  const center = { lat: 65.58456, lng: 22.14785 };
 
   const googleMapsOptions = {
     styles: mapStyles,
     fullscreenControl: false,
-    streetView: false,
     mapTypeControl: false,
   };
 
@@ -30,6 +61,55 @@ function Map({ grid }) {
 
   return (
     <section className={`${styles.container} ${grid}`}>
+      <div className={styles.input_container}>
+        <p>Lat: {coordinates.lat}</p>
+        <p>Lng: {coordinates.lng}</p>
+        <PlacesAutocomplete
+          searchOptions={searchOptions}
+          value={address}
+          onChange={setAddress}
+          onSelect={handleSelect}
+        >
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading,
+          }) => (
+            <div>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search Places ...',
+                  className: 'location-search-input',
+                })}
+              />
+              <div className='autocomplete-dropdown-container'>
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion, index) => {
+                  const className = suggestion.active
+                    ? 'suggestion-item--active'
+                    : 'suggestion-item';
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                  return (
+                    <div
+                      key={index}
+                      {...getSuggestionItemProps(suggestion, {
+                        className,
+                        style,
+                      })}
+                    >
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+      </div>
       <GoogleMap
         zoom={8}
         center={center}
---
+++ components/SearchBox.jsx
@@ -0,0 +1,58 @@
+import styles from '../styles/SearchBox.module.scss';
+import React, { useState } from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
+function SearchBox({ grid }) {
+  const { address, setAddress } = useState('');
+  const [coordinates, setCoordinates] = useState({ lat: null, lng: null });
+
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value);
+    const ll = await getLatLng(results[0]);
+    console.log(ll);
+    setAddress(value);
+    setCoordinates(ll);
+  };
+
+  return (
+    <section className={`${styles.container} ${grid}`}>
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: 'Search Places ...',
+              className: 'location-search-input',
+            })}
+          />
+          <div className='autocomplete-dropdown-container'>
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active
+                ? 'suggestion-item--active'
+                : 'suggestion-item';
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default SearchBox;
